45da08cdcf1d717db98b2a79d7959729bcbcc38c
Larnex <bohdan.asauliuk@gmail.com>
2021-12-22 15:10:02 +0100

    complete responsive main slider

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,5 +1,11 @@
 import { DataService } from './../../services/data.service';
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  OnDestroy,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { SubscriptionLike } from 'rxjs';
 import { Property } from 'src/app/interfaces/property';
@@ -8,18 +14,57 @@ import { Property } from 'src/app/interfaces/property';
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss'],
+  host: {
+    '(window: resize)': 'onWindowResize($event)',
+  },
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent implements OnInit, OnDestroy {
   subscription!: SubscriptionLike | null;
-
-  constructor(private router: Router, private dataService: DataService) {}
   properties!: Property[];
   selectedProperty!: Property;
+  width: number = window.innerWidth;
+  @ViewChild('carouselCell') cellHeight!: ElementRef;
+  intervalId!: any;
+
+  public carouselHeight!: number;
+
+  public cellToShow!: number;
+
+  constructor(private router: Router, private dataService: DataService) {}
 
   ngOnInit(): void {
     this.subscription = this.dataService
       .getProperties()
-      .subscribe((properties) => (this.properties = properties));
+      .subscribe((properties) => {
+        this.properties = properties;
+      });
+
+    if (this.width > 1028) {
+      this.carouselHeight = this.width * 0.35;
+    } else if (this.width > 768) {
+      this.carouselHeight = this.width * 0.45;
+    } else  {
+      this.carouselHeight = this.width * 0.9;
+    }
+
+    this.responsiveCarousel();
+  }
+
+  onWindowResize(event: any) {
+    this.width = event.target.innerWidth;
+    this.carouselHeight = this.cellHeight.nativeElement.clientHeight + 30;
+
+    this.responsiveCarousel();
+  }
+
+  responsiveCarousel() {
+    if (this.width > 1028) {
+      this.cellToShow = 3;
+    } else if (this.width > 768) {
+      this.cellToShow = 2;
+    } else {
+      this.cellToShow = 1;
+    }
   }
 
   ngOnDestroy(): void {
